bc87596e24f7c7c9032dda2e3bcb5bdc4ecd26d0
mac <mac@SBCUSD-MacBook-ProC1MRC0UKDV30.local>
2024-08-10 13:06:21 +0500

    initial commit

---
+++ egreetings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace egreetings.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
